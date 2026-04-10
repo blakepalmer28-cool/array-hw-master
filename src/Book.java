b3a645fa8b2afe7ca0545cd27cf9be55094f3e74
blakepalmer <blake_palmer28@milton.edu>
2026-04-10 14:56:52 -0400

    hi

--- src/Book.java
+++ src/Book.java
@@ -7,15 +7,18 @@ public class Book {
     public String author;
     
     public Book(int pRating){
-        title = "Grandmaster of Demonic Cultivation";
-        author = "MO Xiang Tong Xiu";
+        title = "The Goat";
+        author = "Michael Rosa";
         // TODO: set the instance variable to the parameter
+        rating=pRating;
+        print();
 
     }
 
     public void print(){
         System.out.println("title: " + title);
         System.out.println("author: " + author);
+        System.out.println("rating:"+rating);
         //TODO: use System.out to print the values of the
         //      instance varibles so the user can see the review
 
--- src/Library.java
+++ src/Library.java
@@ -2,22 +2,30 @@ import java.sql.SQLOutput;
 
 public class Library {
 
-    public String owner = "Mx. Bradford";
+
+    public String owner = "Blake Palmer ";
     public Book[] allBooks;
     public static void main(String[] args) {
-        //TODO: Make an instance of the main class ok
+
+        Library Lib=new Library();
     }
 
     public Library(){
 
         System.out.println("Welcome to "+ owner + "'s library!!!!!");
         //TODO: Give the array allBooks a size of 10
+        allBooks = new Book[10];
 
         //TODO: Fill the array allBooks with reviews with random ratings from 0 to 10 inclusive
+        for (int i =0; i< allBooks.length; i++){
+            int randomRating = (int)(Math.random()*11);
+            allbooks.l[i]=randomRating;
+        }
 
         //TODO: print all the ratings in the array allBooks
         
         //TODO: Call the method averageRating
+        averageRating();
 
     }
 
