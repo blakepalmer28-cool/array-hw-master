1c670b3167969c673291932ec432ec10f583786c
blakepalmer <blake_palmer28@milton.edu>
2026-04-11 23:56:34 -0400

    done finally

--- src/Book.java
+++ src/Book.java
@@ -13,6 +13,7 @@ public class Book {
         rating=pRating;
         print();
 
+
     }
 
     public void print(){
--- src/Library.java
+++ src/Library.java
@@ -7,7 +7,7 @@ public class Library {
     public Book[] allBooks;
     public static void main(String[] args) {
 
-        Library Lib=new Library();
+        Library myLibrary=new Library();
     }
 
     public Library(){
@@ -19,17 +19,31 @@ public class Library {
         //TODO: Fill the array allBooks with reviews with random ratings from 0 to 10 inclusive
         for (int i =0; i< allBooks.length; i++){
             int randomRating = (int)(Math.random()*11);
-            allbooks.l[i]=randomRating;
+            allBooks[i]=new Book(randomRating);
         }
 
         //TODO: print all the ratings in the array allBooks
-        
+        for (int i =0; i<allBooks.length;i++){
+               allBooks[i].print();
+        }
+
+
+
         //TODO: Call the method averageRating
         averageRating();
 
     }
 
     public void averageRating(){
+        double sum = 0;
+
+        for (int i = 0; i < allBooks.length; i++) {
+            sum += allBooks[i].rating;
+        }
+        double average = sum/ allBooks.length;
+
+        System.out.println("Average: " + average);
+
         //TODO: calculate the average rating from the array allBooks and print it
 
     }
